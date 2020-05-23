0ce2df43c16e070995d260b0910def38dbf51883
Gabe Jensen <thegabejensen@gmail.com>
2020-05-22 23:42:33 -0700

    Changed h2 tag

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <h2 style={{ textAlign: 'center' }}>Home to the most brutally honest food reviews you'll ever read.</h2>
+    <h2 style={{ textAlign: 'center' }}>Absolutely brutal food reviews.</h2>
   );
 }
 
